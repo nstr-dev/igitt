288a54fb8d8ccc4b7f273004d4de8740be3a4e38
Noah Streller <noah.streller@gmx.ch>
2024-08-20 16:05:19 +0200

    feat: added better ui

--- internal/operations/interactive/interactive.go
+++ internal/operations/interactive/interactive.go
@@ -1,33 +1,80 @@
 package interactive
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"encoding/json"
 	"strings"
 
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/noahstreller/igitt/internal/utilities/logger"
+	"github.com/rivo/uniseg"
 	"github.com/spf13/cobra"
+
+	_ "embed"
 )
 
+//go:embed operations.json
+var commandJSON []byte
+
+const iconWidth = 3
+
+type Command struct {
+	Id            string `json:"id"`
+	Icon          string `json:"icon"`
+	Name          string `json:"name"`
+	Description   string `json:"description"`
+	NextStep      string `json:"nextStep"`
+	NextStepTitle string `json:"nextStepTitle"`
+}
+
+func getTitle(command Command) string {
+	return command.Icon + strings.Repeat(" ", iconWidth-uniseg.StringWidth(command.Icon)) + command.Name
+}
+
 func StartInteractive(rootCmd *cobra.Command) {
-	fmt.Println("Entering interactive mode. Type 'exit' or 'quit' to leave.")
-	reader := bufio.NewReader(os.Stdin)
+	var commands []Command
+	var selectedCommand Command
 
-	for {
-		fmt.Print("> ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
+	var repoUrlInput string
 
-		if input == "exit" || input == "quit" {
-			fmt.Println("Exiting interactive mode.")
-			os.Exit(0)
-		}
+	_ = json.Unmarshal(commandJSON, &commands)
+	commandOptions := make([]huh.Option[Command], len(commands))
 
-		args := strings.Split(input, " ")
-		rootCmd.SetArgs(args)
-
-		if err := rootCmd.Execute(); err != nil {
-			fmt.Println(err)
-		}
+	for i, c := range commands {
+		title := " " + getTitle(c)
+		commandOptions[i] = huh.NewOption(title, c)
 	}
+
+	theme := huh.ThemeCatppuccin()
+	theme.Focused.Base.Border(lipgloss.HiddenBorder())
+
+	err := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[Command]().
+				Title("Igitt").
+				Height(20).
+				DescriptionFunc(func() string {
+					if selectedCommand.NextStep != "none" {
+						return "\n" + getTitle(selectedCommand) + ": " + selectedCommand.Description + "\n\n" + "     " + "Next step: " + selectedCommand.NextStepTitle + "\n"
+					}
+					return "\n" + getTitle(selectedCommand) + ": " + selectedCommand.Description + "\n\n\n"
+				}, &selectedCommand).
+				Options(commandOptions...).
+				Value(&selectedCommand),
+		),
+		huh.NewGroup(huh.NewInput().
+			Title("Link to Git repository").
+			Description("\n  Enter the link to your repository here.\n").
+			Suggestions([]string{"https://github.com"}).
+			Value(&repoUrlInput),
+		).WithHideFunc(func() bool {
+			return selectedCommand.NextStep != "op-enter-repo-url"
+		}),
+	).WithTheme(theme).Run()
+
+	if err != nil {
+		logger.ErrorLogger.Fatal(err)
+	}
+
+	// rootCmd.Print(selectedCommand)
 }
