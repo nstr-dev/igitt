94836222ca907fe673863c14dd0f16358ab39ce5
Noah Streller <noah.streller@gmx.ch>
2024-09-01 00:59:33 +0200

    feat: changed to better alias script ui

--- internal/operations/igt.go
+++ internal/operations/igt.go
@@ -23,8 +23,17 @@ func CreateAliasScripts() {
 if exist `+ executable +` (
 	`+ executable +` %*
 ) else (
-	igitt mkalias
-	igitt %*
+	echo.
+	echo ================ igitt ================
+	echo.
+	echo Run following command to [32mcreate[0m an alias script [34m^(igitt =^> igt^)[0m:
+	echo.
+	echo [36migitt mkalias[0m
+	echo.
+	echo.
+	echo If you don't want this alias, run this to [31mremove[0m this script:
+	echo.
+	echo [36mdel %~dpnx0[0m
 )`
 
 	aliasScriptUnix := `#!/bin/bash
