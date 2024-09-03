e29884a72f064fb0479740635a37fa664240ea71
Noah Streller <noah.streller@gmx.ch>
2024-09-03 14:18:40 +0200

    chore: removed git documentation

--- internal/operations/git/status.go
+++ internal/operations/git/status.go
@@ -24,39 +24,7 @@ type ModifiedStatusInfo struct {
 	StatusLetter string
 }
 
-/*
-
-https://git-scm.com/docs/git-status#_short_format
-
-X          Y     Meaning
--------------------------------------------------
-         [AMD]   not updated
-M        [ MTD]  updated in index
-T        [ MTD]  type changed in index
-A        [ MTD]  added to index
-D                deleted from index
-R        [ MTD]  renamed in index
-C        [ MTD]  copied in index
-[MTARC]          index and work tree matches
-[ MTARC]    M    work tree changed since index
-[ MTARC]    T    type changed in work tree since index
-[ MTARC]    D    deleted in work tree
-	          R    renamed in work tree
-	          C    copied in work tree
--------------------------------------------------
-D           D    unmerged, both deleted
-A           U    unmerged, added by us
-U           D    unmerged, deleted by them
-U           A    unmerged, added by them
-D           U    unmerged, deleted by us
-A           A    unmerged, both added
-U           U    unmerged, both modified
--------------------------------------------------
-?           ?    untracked
-!           !    ignored
--------------------------------------------------
-
-*/
+// https://git-scm.com/docs/git-status#_short_format
 
 var FileStatuses = []ModifiedStatusInfo{
 	// Basic States
